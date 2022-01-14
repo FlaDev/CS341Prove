990d433bf787b09c57993b92c302eec7d2653c3a
FlaDev <flaprumucena@byui.edu>
2022-01-13 20:31:58 -0700

    adjusting app due to errors - week2 prove2

--- prove2/app.js
+++ prove2/app.js
@@ -18,7 +18,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: 'Error' });
+  // res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
