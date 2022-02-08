0bd5d98cc6965d9ec4be812aeeb1396782676c58
FlaDev <prumucena@gmail.com>
2022-02-07 23:58:48 -0700

    prove 5. Following along with videos

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URL = process.env.MONGODB_URL;
 const app = express();
-
+const store = new MongoDBStore({
+    uri: MONGODB_URL,
+    collection: 'sessions'
+  });
+  const csrfProtection = csrf();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const authRoutes = require('./routes/auth');
 
 
 const PORT = process.env.PORT || 5000
@@ -23,7 +31,7 @@ const corsOptions = {
     origin: "https://prove-341.herokuapp.com/",
     optionsSuccessStatus: 200
 };
-const MONGODB_URL = process.env.MONGODB_URL;
+
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -33,19 +41,35 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('61f585951f8ce038f8492349')
-    .then(user => {
-      req.user = user;
-      next();
+app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store
     })
-    .catch(err => console.log(err));
-});
-
+  );
+  app.use(csrfProtection);
+  app.use(flash());
+  app.use((req, res, next) => {
+    if (!req.session.user) {
+      return next();
+    }
+    User.findById(req.session.user._id)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
+  app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 
@@ -55,18 +79,18 @@ mongoose
     MONGODB_URL, options
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'test',
-          email: 'test@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
+    // User.findOne().then(user => {
+    //   if (!user) {
+    //     const user = new User({
+    //       name: 'test',
+    //       email: 'test@test.com',
+    //       cart: {
+    //         items: []
+    //       }
+    //     });
+    //     user.save();
+    //   }
+    // });
     // app.listen(3000);
     app.listen(PORT, () => {
       console.info(`Store Server running on Port ${PORT}`);
@@ -76,3 +100,90 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const path = require('path');
+// const cors = require('cors');
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const session = require('express-session');
+// const MongoDBStore = require('connect-mongodb-session')(session);
+// const csrf = require('csurf');
+// const flash = require('connect-flash');
+
+// const errorController = require('./controllers/error');
+// const User = require('./models/user');
+
+// const MONGODB_URL = process.env.MONGODB_URL;
+
+// const app = express();
+// const store = new MongoDBStore({
+//   uri: MONGODB_URL,
+//   collection: 'sessions'
+// });
+// const csrfProtection = csrf();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+// const authRoutes = require('./routes/auth');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(
+//   session({
+//     secret: 'my secret',
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store
+//   })
+// );
+// app.use(csrfProtection);
+// app.use(flash());
+
+// app.use((req, res, next) => {
+//   if (!req.session.user) {
+//     return next();
+//   }
+//   User.findById(req.session.user._id)
+//     .then(user => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
+
+// app.use((req, res, next) => {
+//   res.locals.isAuthenticated = req.session.isLoggedIn;
+//   res.locals.csrfToken = req.csrfToken();
+//   next();
+// });
+
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+// app.use(authRoutes);
+
+// app.use(errorController.get404);
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(result => {
+//     app.listen(3000);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
