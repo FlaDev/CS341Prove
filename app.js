e53aaff62006b17ccb84b6c8eb492e48ea8979a5
FlaDev <prumucena@gmail.com>
2022-02-08 22:47:33 -0700

    week 6

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URL = process.env.MONGODB_URL;
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URL,
-    collection: 'sessions'
-  });
-  const csrfProtection = csrf();
+  uri: MONGODB_URL,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -42,60 +44,62 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({
-      secret: 'my secret',
-      resave: false,
-      saveUninitialized: false,
-      store: store
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
     })
-  );
-  app.use(csrfProtection);
-  app.use(flash());
-  app.use((req, res, next) => {
-    if (!req.session.user) {
-      return next();
-    }
-    User.findById(req.session.user._id)
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => console.log(err));
-  });
-  app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-  });
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
 
-
+app.use((error, req, res, next) => {
+  
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
-  .connect(
-    MONGODB_URL, options
-  )
+  .connect(MONGODB_URL, options)
   .then(result => {
-    // User.findOne().then(user => {
-    //   if (!user) {
-    //     const user = new User({
-    //       name: 'test',
-    //       email: 'test@test.com',
-    //       cart: {
-    //         items: []
-    //       }
-    //     });
-    //     user.save();
-    //   }
-    // });
-    // app.listen(3000);
     app.listen(PORT, () => {
       console.info(`Store Server running on Port ${PORT}`);
   })
-
   })
   .catch(err => {
     console.log(err);
